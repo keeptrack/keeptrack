f007ed443cda6e69498bb9d226cc8ecd6db28f6d
Bryhon <cath.palug@hotmail.co.uk>
2020-05-27 18:54:34 +0100

    Now loading from html template file

--- hire/views.py
+++ hire/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at hire index")
+    return render(request, 'hire/index.html', {})
