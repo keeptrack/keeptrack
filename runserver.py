d5978b1f64c21e950a366195042687285ff9fe4f
Matej Genci <matej.genci@gmail.com>
2020-05-31 00:29:36 +0200

    Create a run script that sets up env vars
    Usage: ./runserver.py [port_number]

---
+++ runserver.py
@@ -0,0 +1,15 @@
+#!python
+
+import os
+import sys
+
+if os.getenv('VIRTUAL_ENV') == None:
+    print("Start the venv first")
+    exit()
+
+port = sys.argv[1] if len(sys.argv) > 1 else ''
+
+# Setup django environment variables
+os.environ['SECRET_KEY'] = '5TiuyV%f0K*8c!NIyjP$GfPfZ4+#bLdPyg7_ldx1u9N+@&pUD8'
+os.environ['APP_DEBUG']  = '1'
+os.system('python manage.py runserver ' + port)
