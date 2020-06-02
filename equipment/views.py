22150ddaf1f0141586c553887a2fd2f89841fac2
Maksymilian Graczyk <maksymilian.graczyk18@imperial.ac.uk>
2020-06-02 20:27:31 +0200

    Implement the front-end of equipment overview according to the mock-up

--- equipment/views.py
+++ equipment/views.py
@@ -3,4 +3,26 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "equipment/index.html")
+    return render(request, "equipment/index.html", {
+        "equipment_list": [
+            {
+                "uid": 1,
+                "category": "Camera",
+                "brand": "Canon",
+                "name": "9000",
+                "condition": "Good",
+                "next_hire_date": "31 July",
+                "hire_price": "10"
+            },
+            {
+                "uid": 2,
+                "category": "Test",
+                "brand": "Test Brand",
+                "name": "Test Model",
+                "condition": "Out of Order",
+                "next_hire_date": "None",
+                "hire_price": "20"
+            }
+        ]
+    }
+    )
