09da496c6d62d1cd37ca732b5c4bb02633317ae7
Matej Genci <matej.genci@gmail.com>
2020-06-02 20:16:12 +0200

    Use namespacing for url names

--- hire/urls.py
+++ hire/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 
+app_name = 'hire'
 urlpatterns = [path('details', views.details, name='details'),
                path('', views.index, name='index')]
