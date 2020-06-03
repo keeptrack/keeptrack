1e817f5e01d8530dd717ffdc6fbf760c4b574f5c
Matej Genci <matej.genci@gmail.com>
2020-06-03 23:28:32 +0200

    Change models to link hire and assets/equipment

--- hire/models.py
+++ hire/models.py
@@ -11,6 +11,7 @@ class HireRequest(models.Model):
     hire_from = models.DateField(auto_now=False, auto_now_add=False)
     hire_to = models.DateField(auto_now=False, auto_now_add=False)
     description = models.TextField()
+    # TODO: requested_assets = models.ManyToManyField(OfferedServices)
 
     # Metadata.
     # If we're hiring on behalf of society
@@ -19,6 +20,8 @@ class HireRequest(models.Model):
     approved = models.BooleanField(default=False)
     # If we want to reject 
     rejected = models.BooleanField(default=False)
+    # TODO: One hire is associated with many assets.
+    # allocated_assets = models.ManyToManyField(Asset, through='AllocatedEquipment')
 
     def __str__(self):
         return f"Hire({self.name}, {self.hire_from})"
--- keeptrack_hire/models.py
+++ keeptrack_hire/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+from hire.models import HireRequest
+
+class AllocatedEquipment(models.Model):
+    request = models.ForeignKey(HireRequest, on_delete=models.CASCADE)
+    # euqipment = models.ForeignKey(Asset, null=True)
