98d23c94240e97b035dbf7af614d5144d12518c8
Bryhon <cath.palug@hotmail.co.uk>
2020-06-03 16:09:22 +0100

    [bc2918] - Attempt at fixing calendar, need to fix template url

--- eventscalendar/views.py
+++ eventscalendar/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render
-
 # Create your views here.
-from django.utils.safestring import mark_safe
-
-from .utils import Calendar
-from django.views.generic import ListView
-from .models import Event
 from django.urls import reverse_lazy
+from django.utils.safestring import mark_safe
+from django.views.generic import ListView
+
+from .models import Event
+from .utils import Calendar
 
 
 class CalendarView(ListView):
@@ -19,10 +17,22 @@ success_url = reverse_lazy("calendar")
 
 def get_context_data(self):
     context = super().get_queryset()
-    d = get_date(self.request.GET.get('month', None))
+
+    d = self.request.GET.get('month', None)
+
     cal = Calendar(d.year, d.month)
-    html_cal = cal.formatmonth(withyear=True)
+    html_cal = cal.formatmonth(d.year, d.month, withyear=True)
+
     context['calendar'] = mark_safe(html_cal)
     context['prev_month'] = prev_month(d)
     context['next_month'] = next_month(d)
+
     return context
+
+
+def prev_month(d):
+    return d.month - 1
+
+
+def next_month(d):
+    return d.month - 1
