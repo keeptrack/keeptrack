11b028b48e65abfc2fc5652d22997204b05116bc
Maksymilian Graczyk <maksymilian.graczyk18@imperial.ac.uk>
2020-06-03 16:38:09 +0200

    Implement an edit lock in the equipment page based on the user feedback

---
+++ equipment/static/equipment/index.js
@@ -0,0 +1,30 @@
+function checkboxClicked() {
+    var checkbox = window.event.target;
+    var name = checkbox.name;
+    var row = document.getElementById("equip" + name);
+    var disabled = !checkbox.checked;
+    
+    var children = row.childNodes;
+
+    for (var i = 0; i < children.length; i++) {
+	if (children[i].nodeName == "TD") {
+	    var cellChildren = children[i].childNodes;
+	    for (var j = 0; j < cellChildren.length; j++) {
+		if (cellChildren[j].nodeName == "INPUT"
+		    && cellChildren[j].name != name) {
+		    cellChildren[j].disabled = disabled;
+		}
+	    }
+	}
+    }
+}
+
+window.onload = function () {
+    var inputs = document.getElementsByTagName("INPUT");
+
+    for (var i = 0; i < inputs.length; i++) {
+	if (inputs[i].type == "checkbox") {
+	    inputs[i].addEventListener("click", checkboxClicked);
+	}
+    }
+};
