1f0c3d7705d80249e0e10c581da0c32c64327a7a
Maksymilian Graczyk <maksymilian.graczyk18@imperial.ac.uk>
2020-06-03 20:41:01 +0200

    Implement adding new assets in the equipment page

--- equipment/static/equipment/index.js
+++ equipment/static/equipment/index.js
@@ -1,3 +1,8 @@
+const types = ["category", "brand", "name", "condition", "value",
+               "storage_location", "next_hire_date", "hire_price", "notes"];
+
+var lastUid;
+
 function checkboxClicked() {
     var checkbox = window.event.target;
     var name = checkbox.id;
@@ -30,10 +35,7 @@ function matches(input, value) {
 }
 
 function filterChanged() {
-    var types = ["category", "brand", "name", "condition", "value",
-                 "storage_location", "next_hire_date", "hire_price", "notes"];
-
-    var inputs_changed = false;
+    var inputsChanged = false;
     
     types.forEach(function (type, index) {
         var inputs = document.getElementsByName(type);
@@ -44,7 +46,7 @@ function filterChanged() {
         }
         
         for (var i = 1; i < inputs.length; i++) {
-            if (!inputs_changed
+            if (!inputsChanged
                 || inputs[i].parentNode.parentNode.style.display != "none") {
                 if (matches(inputs[i], query)) {
                     inputs[i].parentNode.parentNode.style.display =
@@ -55,15 +57,75 @@ function filterChanged() {
             }
         }
         
-        inputs_changed = true;
+        inputsChanged = true;
     });
 
-    if (!inputs_changed) {
+    var addButton = document.getElementById("add_asset");
+    
+    if (!inputsChanged) {
         var inputs = document.getElementsByTagName("TR");
         for (var i = 0; i < inputs.length; i++) {
             inputs[i].style.display = "table-row";
         }
+
+        addButton.disabled = false;
+    } else {
+        addButton.disabled = true;
     }
+}
+
+function addAssetClicked() {
+    var uid = lastUid + 1;
+    lastUid = uid;
+    
+    var table = document.getElementById("asset_table");
+    var tableRow = document.createElement("tr");
+    tableRow.id = "equip" + uid;
+
+    var firstCell = document.createElement("td");
+    
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.id = uid;
+    checkbox.checked = true;
+    checkbox.addEventListener("click", checkboxClicked);
+
+    var idSpan = document.createElement("span");
+    idSpan.innerText = " " + uid;
+
+    firstCell.appendChild(checkbox);
+    firstCell.appendChild(idSpan);
+
+    tableRow.appendChild(firstCell);
+
+    types.forEach(function (type, index) {
+        var cell = document.createElement("td");
+
+        if (type != "next_hire_date") {
+            var input = document.createElement("input");
+            input.name = type;
+
+            if (type == "value" || type == "hire_price") {
+                input.type = "number";
+                input.value = 0;
+            } else {
+                input.type = "text";
+                input.value = "";
+            }
+
+            cell.appendChild(input);
+        } else {
+            var span = document.createElement("span");
+            span.name = "next_hire_date";
+            span.innerText = "None";
+
+            cell.appendChild(span);
+        }
+        
+        tableRow.appendChild(cell);
+    });
+
+    table.appendChild(tableRow);
 }
 
 window.onload = function () {
@@ -76,4 +138,8 @@ window.onload = function () {
             inputs[i].addEventListener("change", filterChanged);
         }
     }
+
+    document.getElementById("add_asset").addEventListener("click", addAssetClicked);
+
+    lastUid = parseInt(document.getElementById("last_uid").value);
 };
