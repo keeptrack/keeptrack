911631625c2e8d1596b0e5b4e9710e521f891aaa
Maksymilian Graczyk <maksymilian.graczyk18@imperial.ac.uk>
2020-06-03 19:03:33 +0200

    Implement filtering in JavaScript in the equipment page

--- equipment/static/equipment/index.js
+++ equipment/static/equipment/index.js
@@ -1,6 +1,6 @@
 function checkboxClicked() {
     var checkbox = window.event.target;
-    var name = checkbox.name;
+    var name = checkbox.id;
     var row = document.getElementById("equip" + name);
     var disabled = !checkbox.checked;
     
@@ -11,7 +11,7 @@ function checkboxClicked() {
             var cellChildren = children[i].childNodes;
             for (var j = 0; j < cellChildren.length; j++) {
                 if (cellChildren[j].nodeName == "INPUT"
-                    && cellChildren[j].name != name) {
+                    && cellChildren[j].id != name) {
                     cellChildren[j].disabled = disabled;
                 }
             }
@@ -19,8 +19,51 @@ function checkboxClicked() {
     }
 }
 
+function matches(input, value) {
+    if (input.tagName == "SPAN") {
+        return input.innerText.toLowerCase().includes(value.toLowerCase());
+    } else if (input.type == "number") {
+        return input.value == value;
+    } else {
+        return input.value.toLowerCase().includes(value.toLowerCase());
+    }
+}
+
 function filterChanged() {
+    var types = ["category", "brand", "name", "condition", "value",
+                 "storage_location", "next_hire_date", "hire_price", "notes"];
+
+    var inputs_changed = false;
     
+    types.forEach(function (type, index) {
+        var inputs = document.getElementsByName(type);
+        var query = inputs[0].value;
+
+        if (query == "") {
+            return;
+        }
+        
+        for (var i = 1; i < inputs.length; i++) {
+            if (!inputs_changed
+                || inputs[i].parentNode.parentNode.style.display != "none") {
+                if (matches(inputs[i], query)) {
+                    inputs[i].parentNode.parentNode.style.display =
+                        "table-row";
+                } else {
+                    inputs[i].parentNode.parentNode.style.display = "none";
+                }
+            }
+        }
+        
+        inputs_changed = true;
+    });
+
+    if (!inputs_changed) {
+        var inputs = document.getElementsByTagName("TR");
+        for (var i = 0; i < inputs.length; i++) {
+            inputs[i].style.display = "table-row";
+        }
+    }
 }
 
 window.onload = function () {
