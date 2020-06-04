90074a51f95e364133e58f25467bdae97a436be0
Maksymilian Graczyk <maksymilian.graczyk18@imperial.ac.uk>
2020-06-04 12:40:14 +0200

    Implement removing/restoring assets and add a colour explanation

--- equipment/static/equipment/index.js
+++ equipment/static/equipment/index.js
@@ -3,6 +3,9 @@ const types = ["category", "brand", "name", "condition", "value",
 
 var lastUid;
 
+var selectedCount = 0;
+var selectedMarkedCount = 0;
+
 function checkboxClicked() {
     var checkbox = window.event.target;
     var name = checkbox.id;
@@ -22,6 +25,24 @@ function checkboxClicked() {
             }
         }
     }
+
+    var num = disabled ? -1 : 1;
+
+    if (row.classList.contains("deleted")) {
+        selectedMarkedCount += num;
+    } else {
+        selectedCount += num;
+    }
+
+    updateRemoveRestoreButtons();
+}
+
+function updateRemoveRestoreButtons() {
+    var removeButton = document.getElementById("remove_asset");
+    var restoreButton = document.getElementById("restore_asset");
+
+    removeButton.disabled = selectedCount <= 0;
+    restoreButton.disabled = selectedMarkedCount <= 0;
 }
 
 function matches(input, value) {
@@ -126,16 +147,64 @@ function addAssetClicked() {
         tableRow.appendChild(cell);
     });
 
-    tableRow.className = "new";
+    tableRow.classList.add("new");
 
     $row = $(tableRow);
     $("#asset_table").find("tbody").append($row).trigger("addRows", [$row, false]);
+
+    selectedCount += 1;
+    updateRemoveRestoreButtons();
+}
+
+function getSelectedRows() {
+    var rows = document.getElementsByTagName("TR");
+    var rowsToReturn = [];
+
+    for (var i = 0; i < rows.length; i++) {
+        if (rows[i].id.includes("equip")
+           && rows[i].children[0].children[0].checked) {
+            rowsToReturn.push(rows[i]);
+        }
+    }
+
+    return rowsToReturn;
+}
+
+function removeAssetClicked() {
+    var rows = getSelectedRows();
+
+    rows.forEach(function (row, index) {
+        if (row.classList.contains("new")) {
+            document.getElementsByTagName("TBODY")[0].removeChild(row);
+            $("#asset_table").trigger("update");
+        } else if (!row.classList.contains("deleted")) {
+            row.classList.add("deleted");
+            selectedCount -= 1;
+            selectedMarkedCount += 1;
+            updateRemoveRestoreButtons();
+        }
+    });
+}
+
+function restoreAssetClicked() {
+    var rows = getSelectedRows();
+
+    rows.forEach(function (row, index) {
+        if (row.classList.contains("deleted")) {
+            row.classList.remove("deleted");
+            selectedCount += 1;
+            selectedMarkedCount -= 1;
+            updateRemoveRestoreButtons();
+        }
+    });
 }
 
 function update() {
     $("#asset_table").trigger("update");
     var row = window.event.target.parentNode.parentNode;
-    row.className = "updated";
+    if (!row.classList.contains("new")) {
+        row.classList.add("updated");
+    }
 }
 
 window.onload = function () {
@@ -152,6 +221,8 @@ window.onload = function () {
     }
 
     document.getElementById("add_asset").addEventListener("click", addAssetClicked);
+    document.getElementById("remove_asset").addEventListener("click", removeAssetClicked);
+    document.getElementById("restore_asset").addEventListener("click", restoreAssetClicked);
 
     lastUid = parseInt(document.getElementById("last_uid").value);
 
