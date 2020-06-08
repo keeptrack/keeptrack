3c1c8e985ee618e60d18e8ad80f2d7357c097c62
Matej Genci <matej.genci@gmail.com>
2020-06-08 16:34:09 +0200

    Add custom discounted price to hired item

--- keeptrack_hire/models.py
+++ keeptrack_hire/models.py
@@ -6,3 +6,6 @@ from equipment.models import Asset
 class AllocatedEquipment(models.Model):
     request = models.ForeignKey(HireRequest, on_delete=models.CASCADE)
     asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
+
+    discounted_price = models.DecimalField(max_digits=8, decimal_places=2,
+                                           blank=True, null=True)
