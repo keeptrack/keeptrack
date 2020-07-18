7b27053aaaae391b32fff3e6d7202ee65805c8a0
Matej Genci <matej.genci@gmail.com>
2020-07-18 19:45:56 +0200

    Create an app for shared resources

--- keeptrack/settings.py
+++ keeptrack/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hire',
-    'keeptrack.common',
+    'common',
     'keeptrack_hire',
     'widget_tweaks',
     'cal.apps.CalConfig',
